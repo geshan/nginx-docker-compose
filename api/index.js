20b81c9be0e79042b89d88f9b63ba02d6a799371
Geshan Manandhar <geshan@simplywallst.com>
2024-03-12 07:02:04 +1100

    feat: A sample summary of the HTTP article on wikipedia

--- api/index.js
+++ api/index.js
@@ -6,63 +6,10 @@ app.get('/', (req, res) => {
   res.send({message: 'alive'})
 });
 
-app.get('/quotes', (req, res) => {
+app.get('/summary', (req, res) => {
   res.send({
-    data: [
-      {
-      "id": "1",
-      "quote": "There are only two kinds of languages: the ones people complain about and the ones nobody uses.",
-      "author": "Bjarne Stroustrup"
-      },
-      {
-      "id": "2",
-      "quote": "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-      "author": "Martin Fowler"
-      },
-      {
-      "id": "3",
-      "quote": "First, solve the problem. Then, write the code.",
-      "author": "John Johnson"
-      },
-      {
-      "id": "4",
-      "quote": "Java is to JavaScript what car is to Carpet.",
-      "author": "Chris Heilmann"
-      },
-      {
-      "id": "5",
-      "quote": "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
-      "author": "John Woods"
-      },
-      {
-      "id": "6",
-      "quote": "I'm not a great programmer; I'm just a good programmer with great habits.",
-      "author": "Kent Beck"
-      },
-      {
-      "id": "7",
-      "quote": "Truth can only be found in one place: the code.",
-      "author": "Robert C. Martin"
-      },
-      {
-      "id": "8",
-      "quote": "If you have to spend effort looking at a fragment of code and figuring out what it's doing, then you should extract it into a function and name the function after the \"what\".",
-      "author": "Martin Fowler"
-      },
-      {
-      "id": "9",
-      "quote": "The real problem is that programmers have spent far too much time worrying about efficiency in the wrong places and at the wrong times; premature optimization is the root of all evil (or at least most of it) in programming.",
-      "author": "Donald Knuth"
-      },
-      {
-      "id": "10",
-      "quote": "SQL, Lisp, and Haskell are the only programming languages that I’ve seen where one spends more time thinking than typing.",
-      "author": "Philip Greenspun"
-      }
-      ],
-      "meta": {
-      "page": 1
-      }
+    source: 'https://en.wikipedia.org/wiki/HTTP',
+    summary: `HTTP (Hypertext Transfer Protocol) is a request-response protocol for distributed, collaborative, hypermedia information systems. It defines the format of messages that are exchanged between a client and a server. HTTP can be used to transmit text, images, sound, video, and other multimedia files. In addition, it provides a way to link resources to each other, which is the basis of the World Wide Web.\n\nHTTP was developed by Tim Berners-Lee at CERN in 1989. The first version of the protocol, HTTP/0.9, was released in 1991. HTTP/1.0 was released in 1996, and HTTP/1.1 was released in 1997. HTTP/2 was released in 2015, and HTTP/3 was released in 2022.\n\nThe most common use of HTTP is in the World Wide Web. HTTP is used to retrieve web pages from web servers and to send data from web browsers to web servers. However, HTTP can also be used for other purposes, such as file transfer and remote procedure calls.\n\nHTTP is a stateless protocol, which means that each request and response is independent of the previous ones. This makes HTTP easy to implement and use. However, it also means that HTTP cannot be used to track the state of a session.\n\nTo track the state of a session, HTTP can be used with cookies. Cookies are small pieces of data that are stored on the client's computer. Cookies can be used to keep track of a user's preferences, shopping cart, and other information.\n\nHTTP is a fundamental technology for the World Wide Web. It is a simple, yet powerful protocol that can be used to transmit a wide variety of data.`
   });
 });
 
